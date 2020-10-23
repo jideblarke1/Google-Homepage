90b8d5685197a6606fa1c0d3fd90dca333246202
jideblarke1 <realolajide@gmail.com>
2020-10-23 11:53:09 +0100

    work

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 
 // Your JavaScript goes here!
-console.log("Hello, World!")
-console.log (23 + 5)
-console.log (1+2+3+4+5+6)
-console.log 4+6+9 / 77
-
-let a = 10
-let b = 7 * a
-
-console.log(a)
-console.log(9 * a)
-
-
-console.log(b)
-
-var max, actual, percentage;
-
-max = 57;
-actual = max - 13;
-percentage = actual / max;
 
 let string = 'The revolution will not be televised.';
 string;
